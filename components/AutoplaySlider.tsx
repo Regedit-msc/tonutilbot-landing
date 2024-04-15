c6adbc98091a3bbbbe66f390ebe9dc817cb3d0fc
Okoye <okoyedivine145@gmail.com>
2024-04-15 02:26:32 +0100

    autoscroll sllider implemented

---
+++ components/AutoplaySlider.tsx
@@ -0,0 +1,72 @@
+"use client";
+import React, { FC, useEffect } from "react";
+import shopify from "@assets/integrationSlideImages/Shopify.png";
+import PayPal from "@assets/integrationSlideImages/PayPal.png";
+import Stripe from "@assets/integrationSlideImages/Stripe.png";
+import amazon from "@assets/integrationSlideImages/amazon.png";
+import paystack from "@assets/integrationSlideImages/paystack.png";
+import useWindowWidth from "@hooks/useWindowWidth";
+
+interface IProps {}
+
+const AutoplaySlider: FC<IProps> = () => {
+  const images = [shopify, PayPal, Stripe, amazon, paystack];
+  const width = useWindowWidth() || 0;
+
+  return (
+    <div className="flex justify-center flex-col items-center mt-24 lg:mt-14">
+      <div className="relative flex my-4 mx-10 w-[500px] sm:w-[800px] lg:w-[1200px] py-1 overflow-hidden">
+        <span className="absolute bg-gradient-to-r from-myGray via-myGray lg:via-[30%] via-[60%] to-transparent -left-1 w-28 sm:w-36 md:w-28 h-full z-10" />
+        <span className="absolute bg-gradient-to-l from-myGray via-myGray via-[60%] to-transparent -right-1 w-28 sm:w-32 md:w-28 h-full z-10" />
+        <div
+          className={`flex ${width <= 640 && "logos-slide"} ${
+            width > 640 && "logos-slide-sm"
+          }`}
+        >
+          {images.map((_, i) => (
+            <img
+              src={images[i].src}
+              alt=""
+              className="h-6 sm:h-10 lg:mr-[4.485rem]"
+            />
+          ))}
+          {images.map((_, i) => (
+            <img
+              src={images[i].src}
+              alt=""
+              className="h-6 sm:h-10 lg:mr-[4.485rem]"
+            />
+          ))}
+        </div>
+      </div>
+
+      {/* slide two */}
+      <div className="relative flex my-4 mx-10 w-[500px] sm:w-[800px] lg:w-[1200px] py-1 overflow-hidden">
+        <span className="absolute bg-gradient-to-r from-myGray via-myGray lg:via-[30%] via-[60%] to-transparent -left-1 w-28 sm:w-36 md:w-28 h-full z-10" />
+        <span className="absolute bg-gradient-to-l from-myGray via-myGray via-[60%] to-transparent -right-1 w-28 sm:w-32 md:w-28 h-full z-10" />
+        <div
+          className={`flex ${width <= 640 && "logos-slide2"} ${
+            width > 640 && "logos-slide2-sm"
+          }`}
+        >
+          {images.map((_, i) => (
+            <img
+              src={images[i].src}
+              alt=""
+              className="h-6 sm:h-10 lg:mr-[4.485rem]"
+            />
+          ))}
+          {images.map((_, i) => (
+            <img
+              src={images[i].src}
+              alt=""
+              className="h-6 sm:h-10 lg:mr-[4.485rem]"
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AutoplaySlider;
--- components/home_components/ThirdPartyWeb2Payments.tsx
+++ components/home_components/ThirdPartyWeb2Payments.tsx
@@ -3,51 +3,47 @@
 import { animate, motion, useMotionValue } from "framer-motion";
 import useMeasure from "react-use-measure";
 import { GradientText } from "@components/special_text/GradientText";
-import shopify from "@assets/integrationSlideImages/Shopify.png";
-import PayPal from "@assets/integrationSlideImages/PayPal.png";
-import Stripe from "@assets/integrationSlideImages/Stripe.png";
-import amazon from "@assets/integrationSlideImages/amazon.png";
-import paystack from "@assets/integrationSlideImages/paystack.png";
+
 import { useEffect, useState } from "react";
+import AutoplaySlider from "@components/AutoplaySlider";
 
 export const ThirdPartyWeb2Payments = () => {
-  const images = [shopify, PayPal, Stripe, amazon, paystack];
-  const FAST_DURATION = 5;
-  const SLOW_DURATION = 28;
+  //   const FAST_DURATION = 5;
+  //   const SLOW_DURATION = 28;
 
-  const [duration, setDuration] = useState(FAST_DURATION);
-  let [ref, { width }] = useMeasure();
+  //   const [duration, setDuration] = useState(FAST_DURATION);
+  //   let [ref, { width }] = useMeasure();
 
-  const xTranslation = useMotionValue(0);
+  //   const xTranslation = useMotionValue(0);
 
-  const [mustFinish, setMustFinish] = useState(false);
-  const [rerender, setRerender] = useState(false);
+  //   const [mustFinish, setMustFinish] = useState(false);
+  //   const [rerender, setRerender] = useState(false);
 
-  useEffect(() => {
-    let controls;
-    let finalPosition = -width / 2 - 8;
+  //   useEffect(() => {
+  //     let controls;
+  //     let finalPosition = -width / 2 - 8;
 
-    if (mustFinish) {
-      controls = animate(xTranslation, [xTranslation.get(), finalPosition], {
-        ease: "linear",
-        duration: duration * (1 - xTranslation.get() / finalPosition),
-        onComplete: () => {
-          setMustFinish(false);
-          setRerender(!rerender);
-        },
-      });
-    } else {
-      controls = animate(xTranslation, [0, finalPosition], {
-        ease: "linear",
-        duration: duration,
-        repeat: Infinity,
-        repeatType: "loop",
-        repeatDelay: 0,
-      });
-    }
+  //     if (mustFinish) {
+  //       controls = animate(xTranslation, [xTranslation.get(), finalPosition], {
+  //         ease: "linear",
+  //         duration: duration * (1 - xTranslation.get() / finalPosition),
+  //         onComplete: () => {
+  //           setMustFinish(false);
+  //           setRerender(!rerender);
+  //         },
+  //       });
+  //     } else {
+  //       controls = animate(xTranslation, [0, finalPosition], {
+  //         ease: "linear",
+  //         duration: duration,
+  //         repeat: Infinity,
+  //         repeatType: "loop",
+  //         repeatDelay: 0,
+  //       });
+  //     }
 
-    return controls?.stop;
-  }, [rerender, xTranslation, duration, width]);
+  //     return controls?.stop;
+  // }, [rerender, xTranslation, duration, width]);
 
   return (
     <div className="text-white pt-8">
@@ -73,6 +69,7 @@ export const ThirdPartyWeb2Payments = () => {
           TonBot.
         </p>
       </div>
+      <AutoplaySlider />
     </div>
   );
 };
