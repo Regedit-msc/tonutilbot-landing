bdc3bcf18a6c62b263aea89a9ccf1381fe04a6ac
Okoye <okoyedivine145@gmail.com>
2024-04-16 12:31:00 +0100

    nav scroll effect

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,15 +1,35 @@
 /* eslint-disable @next/next/no-img-element */
+"use client";
 import { GradientButton } from "@components/buttons/GradientButton";
 import logo from "@assets/logo-new.png";
 import menu from "@assets/menu.png";
 import tonderLabs from "@assets/TonderLabs.png";
 import { IoIosArrowDown } from "react-icons/io";
+import { useEffect } from "react";
 
 export const Navbar = () => {
+  useEffect(() => {
+    var scrollY = window.scrollY;
+    var newScroll;
+
+    window.addEventListener("scroll", function () {
+      newScroll = window.scrollY;
+      if (scrollY > newScroll) {
+        document.querySelector("nav")?.classList.remove("-translate-y-[100px]");
+      } else {
+        document.querySelector("nav")?.classList.add("-translate-y-[100px]");
+      }
+      scrollY = newScroll;
+    });
+
+    return () => {
+      window.removeEventListener("scroll", newScroll!);
+    };
+  }, []);
   return (
-    <div className="w-full flex justify-center items-center relative">
+    <div className="nv w-full flex justify-center items-center relative">
       <nav
-        className={`top-0 flex justify-between items-center fixed md:static w-full py-3 md:py-7 sm:px-10 lg:px-20 lg:max-w-[70rem] z-[999] backdrop-blur-xl `}
+        className={`top-0 flex justify-between items-center fixed md:static w-full py-3 md:py-7 sm:px-10 lg:px-20 lg:max-w-[70rem] z-[999] backdrop-blur-xl sm:backdrop:blur-none transition-all duration-300 ease-in-out`}
       >
         <div className="flex items-center pl-6 sm:pl-0 sm:py-8 py-4">
           <img src={logo.src} alt="Logo" className="mr-2 h-10" />
