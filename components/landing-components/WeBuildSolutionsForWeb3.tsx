fcbc6cce8e50aebfb8e8f273dabe2ba6671ab804
Okoye <okoyedivine145@gmail.com>
2024-04-19 15:28:02 +0100

    landing page complete and fully responsive

--- components/landing-components/WeBuildSolutionsForWeb3.tsx
+++ components/landing-components/WeBuildSolutionsForWeb3.tsx
@@ -5,16 +5,14 @@ import worlddextop from "@assets/landing-page/WeBuildSolutionsForWeb3/WorldDexto
 import worldTablet from "@assets/landing-page/WeBuildSolutionsForWeb3/World.svg";
 import imageGroup from "@assets/landing-page/WeBuildSolutionsForWeb3/groupImageDextop.svg";
 import { GradientText } from "@components/special_text/GradientText";
+import LandingDarkButton from "@components/buttons/LandingDarkButton";
 
 interface IProps {}
 
 const WeBuildSolutionsForWeb3: FC<IProps> = () => {
   return (
     <Transition>
-      <div
-        className="text-white bg-gradient-to-r from-[#3DB3FC] via-[#5C80FA] to-[#936BF9] 
-      w-full flex justify-center items-center sm:mb-52 sm:mt-[16rem] my-[8.36rem] relative"
-      >
+      <div className="text-white bg-gradient-to-r from-[#3DB3FC] via-[#5C80FA] to-[#936BF9] w-full flex justify-center items-center my-48 py-10 lg:py-[4.5rem] relative px-4 overflow-hidden max-w-[70rem]">
         <img
           src={worlddextop.src}
           alt="Xmen Image"
@@ -27,29 +25,19 @@ const WeBuildSolutionsForWeb3: FC<IProps> = () => {
         />
         {/**left */}
 
-        <div className="max-w-[73.3rem] ml-[2.19rem] mr-[1.69rem] flex flex-col md:flex-row lg:gap-[7rem] gap-[2.18rem] my-[5.69rem]">
+        <div className="max-w-[70rem] ml-[2.19rem] mr-[1.69rem] flex flex-col justify-center items-center md:flex-row lg:gap-[5rem] gap-10 sm:gap-4">
           <div className="flex flex-col w-full">
-            {" "}
-            <button
-              className="bg-gradient-to-b from-transparent
-            to-white/20 shadow-none tracking-[0.4em] rounded-full py-[0.2rem] px-[0.9rem] 
-              items-center text-white gap-2 w-fit transition-all  mb-[1.44rem]
-              md:text-[1.25rem] text-[0.65956rem]"
-            >
+            <button className="bg-white/[0.1] rounded-full py-2 px-3 flex justify-center items-center text-white gap-3 w-fit tracking-widest font-light mb-6 flex-col">
               OUR MISSION
             </button>
-            <GradientText
-              text={
-                <>
-                  We build <br /> solutions for <br /> Web3 on TON
-                </>
-              }
-              className="lg:text-[4.5rem] z-10 leading-[0.95] text-[1.95rem] sm:text-[2.25rem] md:text-[2.75rem] text-white font-bold tracking-tight"
-            />
+
+            <div className="font-semibold text-[3.5rem] sm:text-[3rem] md:text-[3.5rem] lg:text-[4.5rem] text-white leading-[0.85] tracking-tighter">
+              We build <br /> solutions for <br /> Web3 on TON
+            </div>
           </div>
 
-          <div className="flex flex-col w-full">
-            <p className="mb-[2.38rem] text-[0.75rem] md:text-[1.25rem] font-normal">
+          <div className="flex flex-col w-full gap-10">
+            <p className="font-thin lg:text-[1.25rem]">
               TonderLabs is a company dedicated to solving real world problems
               by building utility softwares and applications that help bridge
               the gap between Web2 and Web3, all of which are built on TON
@@ -57,16 +45,15 @@ const WeBuildSolutionsForWeb3: FC<IProps> = () => {
             </p>
 
             <button
-              className={`bg-white shadow-lg rounded-full md:py-3 md:px-5 py-[0.4rem] pl-[0.79rem] pr-[.53rem] flex justify-center 
+              className={`bg-white shadow-lg rounded-full px-4 lg:px-6 py-2 flex justify-center 
               items-center gap-2 w-fit transition-all z-10 `}
             >
               <div
-                className="font-semibold bg-gradient-to-tr from-blue-400 to-indigo-600 bg-clip-text 
-              text-transparent md:text-[1.5625rem] text-[0.82444rem]"
+                className="font-semibold bg-gradient-to-tr from-blue-400 to-purple-600 bg-clip-text 
+              text-transparent tracking-tighter text-[1.25rem] lg:text-[1.5rem]"
               >
                 Contact Us
               </div>
-
               <img src={imageGroup.src} alt="button-image max-md:w-[3.2rem]" />
             </button>
           </div>
